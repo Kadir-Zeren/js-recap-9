b01c94d63eaa4f821bcf433a77c4d544096e6694
kadir-zeren <k4dzeren@gmail.com>
2023-11-22 23:10:45 +0300

    v1

---
+++ app.js
@@ -0,0 +1,20 @@
+const grades = [55, 77, 23, 89, 100, 44, 33, 45];
+let sum = 0;
+
+for (let i = 0; i < grades.length; i++) {
+  sum += grades[i];
+}
+console.log("AVG:", (sum / grades.length).toFixed(2));
+
+const lessThan50 = [];
+const equalOrBiggerThan50 = [];
+for (let j = 0; j < grades.length; j++) {
+  if (grades[j] < 50) {
+    lessThan50.push(grades[j]);
+  } else {
+    equalOrBiggerThan50.push(grades[j]);
+  }
+}
+console.log(lessThan50);
+console.log(equalOrBiggerThan50);
+console.log(grades);

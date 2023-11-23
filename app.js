cf8a57045f296825564366cf29f9186a94aefe1b
kadir-zeren <k4dzeren@gmail.com>
2023-11-23 08:30:26 +0300

    V4

--- app.js
+++ app.js
@@ -32,18 +32,28 @@
 // console.log(equalOrBiggerThan50);
 // console.log(grades);
 
-const students = ["ahmet", "mehmet", "ismet", "saffet", "ahmet", "saffet"];
-const findStudent = (name) => {
-  let counter = 0;
-  for (let student of students) {
-    student === name.toLowerCase() && counter++;
-  }
-  return counter === 0
-    ? `${name} can not be found`
-    : `${name} found ${counter} times`;
-};
-1;
-console.log(findStudent("ahmet"));
-console.log(findStudent("AHMET"));
-console.log(findStudent("ismet"));
-console.log(findStudent("Alihan"));
+// const students = ["ahmet", "mehmet", "ismet", "saffet", "ahmet", "saffet"];
+// const findStudent = (name) => {
+//   let counter = 0;
+//   for (let student of students) {
+//     student === name.toLowerCase() && counter++;
+//   }
+//   return counter === 0
+//     ? `${name} can not be found`
+//     : `${name} found ${counter} times`;
+// };
+// 1;
+// console.log(findStudent("ahmet"));
+// console.log(findStudent("AHMET"));
+// console.log(findStudent("ismet"));
+// console.log(findStudent("Alihan"));
+
+const prices = [250, 150, 300, 500];
+
+for (let i = 0; i < prices.length; i++) {
+  console.log(prices[i]);
+}
+console.log("*********");
+prices.forEach((p) => console.log(p));
+console.log("*********");
+prices.forEach((p, i) => console.log("Deger:", p, "INDIS:", i));

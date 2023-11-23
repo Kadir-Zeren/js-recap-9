58a37d8ac891321a26c94bdf2d1239eb11103226
kadir-zeren <k4dzeren@gmail.com>
2023-11-23 08:57:33 +0300

    v9

--- app.js
+++ app.js
@@ -63,27 +63,35 @@
 // prices.forEach((price) => (sum += price));
 // console.log("SUM:", sum);
 
-let names = ["Mustafa", "Murat", "Ahmet", "Mustafa", "Ayşe", "Canan"];
+// let names = ["Mustafa", "Murat", "Ahmet", "Mustafa", "Ayşe", "Canan"];
 
-const upperName = names.map((name) => name.toLocaleUpperCase());
-console.log(upperName);
-console.log(names);
+// const upperName = names.map((name) => name.toLocaleUpperCase());
+// console.log(upperName);
+// console.log(names);
 
-names.map((name, i, arr) => {
-  arr[i] = name.toLocaleUpperCase();
-});
-console.log(names);
-const euro = 21.78;
-const dolar = 19.8;
-const tlPrices = [100, 150, 100, 50, 80];
-const dolarPrices = tlPrices.map((tl) => Number((tl / dolar).toFixed(2)));
-const euroPrices = tlPrices.map((tl) => Number((tl / euro).toFixed(2)));
+// names.map((name, i, arr) => {
+//   arr[i] = name.toLocaleUpperCase();
+// });
+// console.log(names);
+// const euro = 21.78;
+// const dolar = 19.8;
+// const tlPrices = [100, 150, 100, 50, 80];
+// const dolarPrices = tlPrices.map((tl) => Number((tl / dolar).toFixed(2)));
+// const euroPrices = tlPrices.map((tl) => Number((tl / euro).toFixed(2)));
 
-console.log(dolarPrices);
-console.log(euroPrices);
-console.log(tlPrices);
+// console.log(dolarPrices);
+// console.log(euroPrices);
+// console.log(tlPrices);
 
 const salaries = [5500, 8000, 6500, 9000, 10000, 15000, 25000];
-const biggerThan = salaries.filter((salary) => salary >= 10000);
-console.log(biggerThan);
-console.log(salaries);
+// const biggerThan = salaries.filter((salary) => salary >= 10000);
+// console.log(biggerThan);
+// console.log(salaries);
+
+const range = salaries.filter((salary) => salary >= 8000 && salary <= 10000);
+console.log(range);
+
+const biggerThan9000 = salaries
+  .filter((s) => s < 9000)
+  .map((s) => Number((s * 1.1).toFixed(2)));
+console.log(biggerThan9000);

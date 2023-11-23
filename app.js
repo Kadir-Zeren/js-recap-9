a448be32f148c5cc56846423d81ad954ff89d557
kadir-zeren <k4dzeren@gmail.com>
2023-11-23 08:19:58 +0300

    V3

--- app.js
+++ app.js
@@ -19,15 +19,31 @@
 // console.log(equalOrBiggerThan50);
 // console.log(grades);
 
-const grades = [55, 77, 23, 89, 100, 44, 33, 45];
+// const grades = [55, 77, 23, 89, 100, 44, 33, 45];
 
-const lessThan50 = [];
-const equalOrBiggerThan50 = [];
-for (let i in grades) {
-  grades[i] < 50
-    ? lessThan50.push(grades[i])
-    : equalOrBiggerThan50.push(grades[i]);
-}
-console.log(lessThan50);
-console.log(equalOrBiggerThan50);
-console.log(grades);
+// const lessThan50 = [];
+// const equalOrBiggerThan50 = [];
+// for (let i in grades) {
+//   grades[i] < 50
+//     ? lessThan50.push(grades[i])
+//     : equalOrBiggerThan50.push(grades[i]);
+// }
+// console.log(lessThan50);
+// console.log(equalOrBiggerThan50);
+// console.log(grades);
+
+const students = ["ahmet", "mehmet", "ismet", "saffet", "ahmet", "saffet"];
+const findStudent = (name) => {
+  let counter = 0;
+  for (let student of students) {
+    student === name.toLowerCase() && counter++;
+  }
+  return counter === 0
+    ? `${name} can not be found`
+    : `${name} found ${counter} times`;
+};
+1;
+console.log(findStudent("ahmet"));
+console.log(findStudent("AHMET"));
+console.log(findStudent("ismet"));
+console.log(findStudent("Alihan"));
